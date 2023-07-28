3d75875b01e2f4a4fba5372102f8912ab774822e
shengjianwen1 <2605484622@qq.com>
2023-07-28 14:10:36 +0800

    初始项目结构

---
+++ sfc-xxljob-service/src/main/java/hk/sfc/XxljobApplication.java
@@ -0,0 +1,24 @@
+package hk.sfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @Description 旭瑶&小滴课堂 xdclass.net
+ * @Author 二当家小D  代码、笔记和技术指导联系我即可
+ * @Version 1.0
+ **/
+
+@SpringBootApplication
+
+
+public class XxljobApplication {
+
+    public static void main(String [] args){
+
+        SpringApplication.run(XxljobApplication.class,args);
+    }
+
+
+}
